1f1f8c2c8269b5750aa0a49f25ede40388abfd7e
alia49 <aaltimimi01@gmail.com>
2024-11-02 14:24:55 -0700

    few changes commented out old code just incase

--- src/application/model/TransBean.java
+++ src/application/model/TransBean.java
@@ -1,97 +1,212 @@
-
 package application.model;
 
 import java.time.LocalDate;
+import java.io.Serializable;
+import java.util.Objects;
 
-public class TransBean {
-    private String accountName;          // Name of the account associated with the transaction
-    private String transactionTypeName;  // Type of transaction (e.g., Education, House)
-    private LocalDate transactionDate;   // Date of the transaction
-    private String transactionDescription; // Description of the transaction
-    private double paymentAmount;        // Payment amount (if applicable)
-    private double depositAmount;        // Deposit amount (if applicable)
+/**
+ * Represents a financial transaction in the Budget Buddy application.
+ */
+public class TransBean implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-    // Constructor
-    public TransBean(String accountName, String transactionTypeName, LocalDate transactionDate, 
-                     String transactionDescription, double paymentAmount, double depositAmount) {
-        this.accountName = accountName;
-        this.transactionTypeName = transactionTypeName;
-        this.transactionDate = transactionDate;
-        this.transactionDescription = transactionDescription;
-        this.paymentAmount = paymentAmount;
-        this.depositAmount = depositAmount;
+    private int transId;
+    private TransTypeBean transType;  // Reference to TransTypeBean instead of String
+    private double amount;
+    private LocalDate date;
+    private String description;
+
+    // Default constructor
+    public TransBean() {}
+
+    // Parameterized constructor
+    public TransBean(int transId, TransTypeBean transType, double amount, LocalDate date, String description) {
+        this.transId = transId;
+        this.transType = transType;
+        this.amount = amount;
+        this.date = date;
+        this.description = description;
     }
 
-    // Getter for account name
-    public String getAccountName() {
-        return accountName;
+    // Getter and Setter for transId
+    public int getTransId() {
+        return transId;
     }
 
-    // Setter for account name
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
+    public void setTransId(int transId) {
+        this.transId = transId;
     }
 
-    // Getter for transaction type name
-    public String getTransactionTypeName() {
-        return transactionTypeName;
+    // Getter and Setter for transType
+    public TransTypeBean getTransType() {
+        return transType;
     }
 
-    // Setter for transaction type name
-    public void setTransactionTypeName(String transactionTypeName) {
-        this.transactionTypeName = transactionTypeName;
+    public void setTransType(TransTypeBean transType) {
+        this.transType = transType;
     }
 
-    // Getter for transaction date
-    public LocalDate getTransactionDate() {
-        return transactionDate;
+    // Getter and Setter for amount
+    public double getAmount() {
+        return amount;
     }
 
-    // Setter for transaction date
-    public void setTransactionDate(LocalDate transactionDate) {
-        this.transactionDate = transactionDate;
+    public void setAmount(double amount) {
+        this.amount = amount;
     }
 
-    // Getter for transaction description
-    public String getTransactionDescription() {
-        return transactionDescription;
+    // Getter and Setter for date
+    public LocalDate getDate() {
+        return date;
     }
 
-    // Setter for transaction description
-    public void setTransactionDescription(String transactionDescription) {
-        this.transactionDescription = transactionDescription;
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 
-    // Getter for payment amount
-    public double getPaymentAmount() {
-        return paymentAmount;
+    // Getter and Setter for description
+    public String getDescription() {
+        return description;
     }
 
-    // Setter for payment amount
-    public void setPaymentAmount(double paymentAmount) {
-        this.paymentAmount = paymentAmount;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    // Getter for deposit amount
-    public double getDepositAmount() {
-        return depositAmount;
+    // Validates the TransBean instance
+    public boolean isValid() {
+        return transId > 0 &&
+               transType != null && transType.isValid() &&
+               amount >= 0 &&
+               date != null;
     }
 
-    // Setter for deposit amount
-    public void setDepositAmount(double depositAmount) {
-        this.depositAmount = depositAmount;
+    // Overrides equals method for object comparison
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TransBean transBean = (TransBean) o;
+        return transId == transBean.transId &&
+               Double.compare(transBean.amount, amount) == 0 &&
+               Objects.equals(transType, transBean.transType) &&
+               Objects.equals(date, transBean.date) &&
+               Objects.equals(description, transBean.description);
     }
 
-    // toString method to display transaction details
+    // Overrides hashCode method
+    @Override
+    public int hashCode() {
+        return Objects.hash(transId, transType, amount, date, description);
+    }
+
+    // Overrides toString method for readable representation
     @Override
     public String toString() {
         return "TransBean{" +
-                "accountName='" + accountName + '\'' +
-                ", transactionTypeName='" + transactionTypeName + '\'' +
-                ", transactionDate=" + transactionDate +
-                ", transactionDescription='" + transactionDescription + '\'' +
-                ", paymentAmount=" + paymentAmount +
-                ", depositAmount=" + depositAmount +
-                '}';
+               "transId=" + transId +
+               ", transType=" + transType +
+               ", amount=" + amount +
+               ", date=" + date +
+               ", description='" + description + '\'' +
+               '}';
     }
 }
+
+//
+//package application.model;
+//
+//import java.time.LocalDate;
+//
+//public class TransBean {
+//    private String accountName;          // Name of the account associated with the transaction
+//    private String transactionTypeName;  // Type of transaction (e.g., Education, House)
+//    private LocalDate transactionDate;   // Date of the transaction
+//    private String transactionDescription; // Description of the transaction
+//    private double paymentAmount;        // Payment amount (if applicable)
+//    private double depositAmount;        // Deposit amount (if applicable)
+//
+//    // Constructor
+//    public TransBean(String accountName, String transactionTypeName, LocalDate transactionDate, 
+//                     String transactionDescription, double paymentAmount, double depositAmount) {
+//        this.accountName = accountName;
+//        this.transactionTypeName = transactionTypeName;
+//        this.transactionDate = transactionDate;
+//        this.transactionDescription = transactionDescription;
+//        this.paymentAmount = paymentAmount;
+//        this.depositAmount = depositAmount;
+//    }
+//
+//    // Getter for account name
+//    public String getAccountName() {
+//        return accountName;
+//    }
+//
+//    // Setter for account name
+//    public void setAccountName(String accountName) {
+//        this.accountName = accountName;
+//    }
+//
+//    // Getter for transaction type name
+//    public String getTransactionTypeName() {
+//        return transactionTypeName;
+//    }
+//
+//    // Setter for transaction type name
+//    public void setTransactionTypeName(String transactionTypeName) {
+//        this.transactionTypeName = transactionTypeName;
+//    }
+//
+//    // Getter for transaction date
+//    public LocalDate getTransactionDate() {
+//        return transactionDate;
+//    }
+//
+//    // Setter for transaction date
+//    public void setTransactionDate(LocalDate transactionDate) {
+//        this.transactionDate = transactionDate;
+//    }
+//
+//    // Getter for transaction description
+//    public String getTransactionDescription() {
+//        return transactionDescription;
+//    }
+//
+//    // Setter for transaction description
+//    public void setTransactionDescription(String transactionDescription) {
+//        this.transactionDescription = transactionDescription;
+//    }
+//
+//    // Getter for payment amount
+//    public double getPaymentAmount() {
+//        return paymentAmount;
+//    }
+//
+//    // Setter for payment amount
+//    public void setPaymentAmount(double paymentAmount) {
+//        this.paymentAmount = paymentAmount;
+//    }
+//
+//    // Getter for deposit amount
+//    public double getDepositAmount() {
+//        return depositAmount;
+//    }
+//
+//    // Setter for deposit amount
+//    public void setDepositAmount(double depositAmount) {
+//        this.depositAmount = depositAmount;
+//    }
+//
+//    // toString method to display transaction details
+//    @Override
+//    public String toString() {
+//        return "TransBean{" +
+//                "accountName='" + accountName + '\'' +
+//                ", transactionTypeName='" + transactionTypeName + '\'' +
+//                ", transactionDate=" + transactionDate +
+//                ", transactionDescription='" + transactionDescription + '\'' +
+//                ", paymentAmount=" + paymentAmount +
+//                ", depositAmount=" + depositAmount +
+//                '}';
+//    }
+//}
--- src/application/model/TransTypeBean.java
+++ src/application/model/TransTypeBean.java
@@ -1,16 +1,107 @@
 package application.model;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Represents a transaction type in the Budget Buddy application.
+ */
+public class TransTypeBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+
+    // Static list to mimic database or storage
+    private static List<TransTypeBean> transTypes = new ArrayList<>();
+
+    // Default constructor
+    public TransTypeBean() {
+    }
+
+    // Parameterized constructor
+    public TransTypeBean(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getter for id
+    public int getId() {
+        return id;
+    }
+
+    // Setter for id
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Validates the TransTypeBean instance
+    public boolean isValid() {
+        return id > 0 && name != null && !name.trim().isEmpty();
+    }
+
+    // Load all transaction types from static list
+    public static List<TransTypeBean> loadTransTypes() {
+        // This method would typically load from a database or external file
+        return new ArrayList<>(transTypes);
+    }
+
+    // Save the transaction type to the static list
+    public boolean saveTransType() {
+        if (this.isValid()) {
+            transTypes.add(this);
+            return true;
+        }
+        return false;
+    }
+
+    // Overrides equals method for object comparison
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TransTypeBean that = (TransTypeBean) o;
+        return id == that.id && Objects.equals(name, that.name);
+    }
+
+    // Overrides hashCode method
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    // Overrides toString method for readable representation
+    @Override
+    public String toString() {
+        return "TransTypeBean{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
+/* Simplified 
+ package application.model;
+
 public class TransTypeBean {
-    // Fields for transaction type information
+    // Field for transaction type name
     private String transTypeName;  // Name of the transaction type (e.g., Deposit, Withdrawal)
-    private String transDescription; // Description of the transaction type
-    private int transCode;         // Unique identifier or code for the transaction type
 
     // Constructor
-    public TransTypeBean(String transTypeName, String transDescription, int transCode) {
+    public TransTypeBean(String transTypeName) {
         this.transTypeName = transTypeName;
-        this.transDescription = transDescription;
-        this.transCode = transCode;
     }
 
     // Getter for transaction type name
@@ -23,33 +114,71 @@ public class TransTypeBean {
         this.transTypeName = transTypeName;
     }
 
-    // Getter for transaction description
-    public String getTransDescription() {
-        return transDescription;
-    }
-
-    // Setter for transaction description
-    public void setTransDescription(String transDescription) {
-        this.transDescription = transDescription;
-    }
-
-    // Getter for transaction code
-    public int getTransCode() {
-        return transCode;
-    }
-
-    // Setter for transaction code
-    public void setTransCode(int transCode) {
-        this.transCode = transCode;
-    }
-
     // toString method to display transaction type details
     @Override
     public String toString() {
         return "TransTypeBean{" +
                 "transTypeName='" + transTypeName + '\'' +
-                ", transDescription='" + transDescription + '\'' +
-                ", transCode=" + transCode +
                 '}';
     }
 }
+
+ */
+
+//
+//package application.model;
+//
+//public class TransTypeBean {
+//    // Fields for transaction type information
+//    private String transTypeName;  // Name of the transaction type (e.g., Deposit, Withdrawal)
+//    private String transDescription; // Description of the transaction type
+//    private int transCode;         // Unique identifier or code for the transaction type
+//
+//    // Constructor
+//    public TransTypeBean(String transTypeName, String transDescription, int transCode) {
+//        this.transTypeName = transTypeName;
+//        this.transDescription = transDescription;
+//        this.transCode = transCode;
+//    }
+//
+//    // Getter for transaction type name
+//    public String getTransTypeName() {
+//        return transTypeName;
+//    }
+//
+//    // Setter for transaction type name
+//    public void setTransTypeName(String transTypeName) {
+//        this.transTypeName = transTypeName;
+//    }
+//
+//    // Getter for transaction description
+//    public String getTransDescription() {
+//        return transDescription;
+//    }
+//
+//    // Setter for transaction description
+//    public void setTransDescription(String transDescription) {
+//        this.transDescription = transDescription;
+//    }
+//
+//    // Getter for transaction code
+//    public int getTransCode() {
+//        return transCode;
+//    }
+//
+//    // Setter for transaction code
+//    public void setTransCode(int transCode) {
+//        this.transCode = transCode;
+//    }
+//
+//    // toString method to display transaction type details
+//    @Override
+//    public String toString() {
+//        return "TransTypeBean{" +
+//                "transTypeName='" + transTypeName + '\'' +
+//                ", transDescription='" + transDescription + '\'' +
+//                ", transCode=" + transCode +
+//                '}';
+//    }
+//}
+
