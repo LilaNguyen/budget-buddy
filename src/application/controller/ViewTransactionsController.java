d3c419f16c8b8c2e40fc85f669188ed513d8673d
Noah Jones <noahj1893@gmail.com>
2024-11-12 01:17:32 -0800

    updated viewTransactionsPage and updated fxml

--- src/application/controller/ViewTransactionsController.java
+++ src/application/controller/ViewTransactionsController.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 import application.dal.DalInt;
 import application.dal.FileDal;
-import application.model.AccountBean;
+import application.model.TransBean;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -14,33 +14,34 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ViewTransactionsController {
 	// Table and the columns
-    @FXML private TableView<AccountBean> transactionTable;
-    @FXML private TableColumn<AccountBean, String> accountColumn;
-    @FXML private TableColumn<AccountBean, String> typeColumn;
-    @FXML private TableColumn<AccountBean, LocalDate> dateColumn;
-    @FXML private TableColumn<AccountBean, String> descriptionColumn;
-    @FXML private TableColumn<AccountBean, Double> amountColumn;
+    @FXML private TableView<TransBean> transactionTable;
+    @FXML private TableColumn<TransBean, String> accountColumn;
+    @FXML private TableColumn<TransBean, String> typeColumn;
+    @FXML private TableColumn<TransBean, LocalDate> dateColumn;
+    @FXML private TableColumn<TransBean, String> descriptionColumn;
+    @FXML private TableColumn<TransBean, Double> paymentColumn;
+    @FXML private TableColumn<TransBean, Double> depositColumn;
+
     
     // Reference to DalInt
     private DalInt dalInterface = new FileDal();
-    // Make it AccountBean
     
-    //private ObservableList<TransactionBean> transactionList;
+    private ObservableList<TransBean> transactionList;
     
     @FXML
     public void initialize() {
         // Set up the columns MAKE SURE NAMES MATCH GET/SET METHODS
-    	accountColumn.setCellValueFactory(new PropertyValueFactory<>("accountName"));
-    	typeColumn.setCellValueFactory(new PropertyValueFactory<>("transactionType"));
-    	dateColumn.setCellValueFactory(new PropertyValueFactory<>("latestDate"));
+    	accountColumn.setCellValueFactory(new PropertyValueFactory<>("account"));
+    	typeColumn.setCellValueFactory(new PropertyValueFactory<>("transType"));
+    	dateColumn.setCellValueFactory(new PropertyValueFactory<>("transDate"));
     	descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
-    	//not sure if best way to incorporate deposit/payment
-    	amountColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));	
+    	paymentColumn.setCellValueFactory(new PropertyValueFactory<>("payment"));	
+    	depositColumn.setCellValueFactory(new PropertyValueFactory<>("deposit"));	
 
         // Initialize the list and table
     	
-    	// transactionList = FXCollections.observableArrayList(dalInterface.loadTransactions());
-    	//transactionTable.setItems(transactionList);
+    	transactionList = FXCollections.observableArrayList(dalInterface.loadTransactions());
+    	transactionTable.setItems(transactionList);
 
         // Sort the data by opening date in descending order
     	dateColumn.setSortType(TableColumn.SortType.DESCENDING);
@@ -50,7 +51,7 @@ public class ViewTransactionsController {
         transactionTable.sort();
         
         // For Debugging-- make sure data is being loaded
-        //System.out.println("Loaded accounts: " + transactionList.size());
+        System.out.println("Loaded accounts: " + transactionList.size());
     }
     
 }
