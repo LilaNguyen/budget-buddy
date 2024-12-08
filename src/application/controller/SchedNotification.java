16a63c5b85ecf41927cf186a163760a7070d82cb
Rogelio-Comino <comino.rogelio@gmail.com>
2024-12-07 16:31:04 -0800

    Implemented message for due payments

---
+++ src/application/controller/SchedNotification.java
@@ -0,0 +1,53 @@
+package application.controller;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import application.model.ScheduledTransBean;
+import application.dal.FileDal;
+
+import java.util.List;
+
+public class SchedNotification {
+
+    /**
+     * Checks for scheduled transactions due today and displays a notification if any exist.
+     *
+     * @param ownerStage the stage to which the alert is linked.
+     * @param fileDal    the data access layer instance.
+     */
+    public static void checkAndAlert(Stage ownerStage, FileDal fileDal) {
+        // Fetch scheduled transactions due today
+        List<ScheduledTransBean> dueToday = fileDal.getDueTransactionsForToday();
+
+        // If there are transactions due today, show an alert
+        if (!dueToday.isEmpty()) {
+            showAlert(ownerStage, dueToday);
+        }
+    }
+
+    /**
+     * Displays an alert with the details of due transactions.
+     *
+     * @param ownerStage the stage to which the alert is linked.
+     * @param dueToday   the list of transactions due today.
+     */
+    private static void showAlert(Stage ownerStage, List<ScheduledTransBean> dueToday) {
+        StringBuilder message = new StringBuilder("You have the following payments due today:\n");
+
+        // Format each scheduled transaction into the message
+        for (ScheduledTransBean trans : dueToday) {
+            message.append("- ").append(trans.getScheduleName())
+                   .append(" (Account: ").append(trans.getAccount()).append(", Amount: $")
+                   .append(trans.getPaymentAmount()).append(")\n");
+        }
+
+        // Create and display the alert
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.initOwner(ownerStage);
+        alert.setTitle("Scheduled Payments Due");
+        alert.setHeaderText("Payment Reminder");
+        alert.setContentText(message.toString());
+        alert.showAndWait();
+    }
+}
